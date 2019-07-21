6cfc67eb8af93657791148ba673a4cf07a9a056c
UIHYEON.CHA.OSX <oliver56@naver.com>
2019-07-22 02:14:04 +0900

    앱 스크립트 수정

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ import 'github-markdown-css/github-markdown.css'
 import './css/app.scss'
 
 if (typeof window.SaSaSakJs !== 'undefined') {
-    document.querySelectorAll('.play-zone').forEach(playZone => {
+    document.querySelectorAll('.play-zone').forEach((playZone) => {
+        let options = {}
+
+        if (playZone.classList.contains('ticket')) {
+            options.strokeMinWidth = 2
+            options.strokeMaxWidth = 5
+            options.strokeMinRotate = -10
+            options.strokeMaxRotate = 30
+            options.maxCountOfOnceScratch = 500
+        }
+
         new SaSaSakJs(playZone.querySelector('.sasasak'), {
+            ...options,
             mounted() {
                 playZone.classList.add('mounted')
                 playZone.querySelector('.play').addEventListener('click', () => {
