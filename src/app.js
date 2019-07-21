6b8d099ddc1a202e1d47e5c7a23cc4e05bdf478d
UIHYEON.CHA.OSX <oliver56@naver.com>
2019-07-22 02:55:18 +0900

    데모페이지 수정

--- src/app.js
+++ src/app.js
@@ -6,11 +6,12 @@ if (typeof window.SaSaSakJs !== 'undefined') {
         let options = {}
 
         if (playZone.classList.contains('ticket')) {
-            options.strokeMinWidth = 2
-            options.strokeMaxWidth = 5
-            options.strokeMinRotate = -10
-            options.strokeMaxRotate = 30
-            options.maxCountOfOnceScratch = 500
+            options.strokeMinWidth = 6
+            options.strokeMaxWidth = 10
+            options.strokeMinRotate = 0
+            options.strokeMaxRotate = 20
+            options.maxCountOfOnceScratch = 600
+            options.completionRate = 0.4
         }
 
         new SaSaSakJs(playZone.querySelector('.sasasak'), {
